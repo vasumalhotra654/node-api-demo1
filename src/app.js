b7f8200bb5d22adeee29bffb1ddb5dff82d70950
Vasu Malhotra <vasumalhotra654@gmail.com>
2025-09-18 14:56:51 +0530

    ds

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 // Health endpoint
 app.get("/health", (req, res) => {
-  res.json({ status: "API is healthy 🚀! Hurr45454dfdfdsfsfseyyy!!!!! Beanstalk is working" });
+  res.json({ status: "API is healthy 🚀! - hurrrryyyyyyyy !" });
 });
 
 // Routes
